eab33f653a8ba1f3e28bc7767f2a041d14cabc9d
deepx223 <90983788+deepx223@users.noreply.github.com>
2024-04-13 11:43:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "Script by Sathira Sri Sathsara (SaM)"
 
 # Update the system and install dependencies
 sudo dpkg --add-architecture i386
@@ -13,7 +12,9 @@ sudo apt install python3-pip
 sudo pip3 install gdown
 
 # Download the server files
-cd /ubuntu
+cd /ubuntu/
+mkdir cod4
+cd /cod4/
 gdown --id 1YhrP8OV0l-LKJKHC6BoRg7pQ1Bki4zYd
 unzip cod4x-linux-server.zip
 rm cod4x-linux-server.zip
@@ -23,4 +24,3 @@ cd /ubuntu/cod4x-linux-server
 sudo chmod 777 *
 sudo screen -S cod4x-server ./start.sh
 
-echo "Server Started Enjoy <3 [SaM]"
