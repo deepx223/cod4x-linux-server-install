7692a734dc3fe6e729dd0d72e6e34dfbb7ad50eb
deepx223 <90983788+deepx223@users.noreply.github.com>
2024-04-13 11:55:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,7 @@ sudo pip3 install gdown
 
 # Download the server files
 cd ..
-mkdir cod4
-cd cod4
+cd cod4x-linux-server
 gdown --id 1YhrP8OV0l-LKJKHC6BoRg7pQ1Bki4zYd
 unzip cod4x-linux-server.zip
 rm cod4x-linux-server.zip
